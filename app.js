972da4fe470216ebcdaea834a0bad89569eda824
ElenaTsap <elenatsapaki@gmail.com>
2021-07-11 20:22:49 +0200

    commit

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const app = express();
-
 //socket does not accept app as a server - it needs a bigger object.
 //so we have to go outside of express and create a new bigger server
-
 const http = require('http');
 const server = http.createServer(app);
-const port = process.env.PORT || 8080;
-
+const port = process.env.PORT || 8081;
 //creating the socket server
 const { Server } = require('socket.io');
 const io = new Server(server);
-
 const jwt = require('jsonwebtoken');
 
 app.use(express.static(__dirname + '/public'));
 
+const usersMap = {};
+const users = [];
+const channels = ['Home', 'Park', 'Pool', 'Lake', 'Square'];
 //for accessing index.html you do not have to specify in the url
 // you can just write http://localhost:8080
 
 io.on('connection', (socket) => {
-    //when a user connects, we create a unique nickname and emit
 
+    socket.emit('channels', channels);
+    //when a user connects, we create a unique nickname and emit
     socket.on('token', token => {
         const ticketHandler = (nickName) => {
             if (!nickName) {
@@ -29,7 +29,11 @@ io.on('connection', (socket) => {
             } 
             const token = jwt.sign({nickName}, 'ahguy21367278@#$%$%@wer', {expiresIn: '1w'});
             const ticket = {token, nickName};
-            console.log(nickName + ' is joind to the chat');
+            console.log(nickName + ' joined the chat!');
+            let findUser = users.find(user => user == nickName);
+            !findUser ? users.push(nickName):'';
+            usersMap[nickName] = socket;
+            socket.userName = nickName;
             return ticket
         }
 
@@ -51,15 +55,28 @@ io.on('connection', (socket) => {
         } else {
             socket.emit('ticket', ticketHandler());
         }
+        io.emit('users', users);
     });
 
-    socket.on('chat message', (message) => {
-        console.log(message);
-        io.emit('chat msg', message);
+    socket.on('chat message', (envelope) => {
+        console.log('envelope', envelope);
+        if (envelope.list == 'users-list') {
+            socket.emit('chat msg', envelope);
+            usersMap[envelope.to].emit('chat msg', envelope);
+        } else {
+            io.emit('chat msg', envelope);
+        }
     })
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected', socket.userName);
+        users.map ((user, index) => {
+            if(socket.userName == user) {
+                users.splice(index, 1)
+                return
+            } 
+        })
+        io.emit('users', users)
     })
 });
 
--- public/main.js
+++ public/main.js
@@ -1,20 +1,109 @@
+//TODO: fix chat between 2 users
 const socket = io();
+
+const $ = selector => document.querySelector(selector);
+
+const state = {
+    currentUser: '',
+    list: 'channels-list',
+    users: [],
+    channels: [],
+    activeChat:'Home',
+    usedChats: {Home:[]}
+}
+
+//managing the channels/users list
+const listManager = (listItems, listType) => {
+    $(`#${listType}`).innerHTML = '';
+    listItems.forEach(item => {
+        if (item!=state.currentUser) {
+            $(`#${listType}`).insertAdjacentHTML('beforeend', `<li id="${item}" onclick="listClickHandler('${item}', '${listType}')" class ="list-item">${item}</li>`);
+        }
+        
+        if (item == listItems[listItems.length-1] && state.list === 'channels-list') {
+            listClickHandler(state.activeChat, 'channels-list');
+        }
+    });
+}
+
+const listClickHandler = (name, listType) => {
+    if (listType == 'channels-list') {
+        state.list = 'channels-list'
+    } else {
+        state.list = 'users-list'
+    }
+    
+    $(`#${name}`).classList.remove('unread');
+
+    const preItem = state.activeChat;
+    state.activeChat = name;
+    $(`#${preItem}`).classList.remove('active');
+    $(`#${name}`).classList.add('active');
+    
+/*     if(!state.usedChats[name] && listType == 'users-list'){
+        state.usedChats[name] = [];
+    } */
+    console.log('usedChats', state.usedChats)
+    $('#chat-box').innerHTML = '';
+    state.usedChats[state.activeChat].forEach( msg => {
+        $('#chat-box').insertAdjacentHTML("beforeend", `<li>${msg}</li>`);
+    });
+
+    console.log('state', state);
+}
+
+//send the message
 const clickHandler = () => {
-    let msg = document.querySelector('#msg-box');
-    socket.emit('chat message', msg.value);
+    let msg = $('#msg-box');
+    socket.emit('chat message', {list: state.list, from:state.currentUser, to:state.activeChat, msg:msg.value});
     msg.value = '';
 }
+
+socket.on('chat msg', envelope => {
+//todo there is something wrong here --- envelope.chat and envelope.from....
+
+//* find if the chat is in the used chats
+    const chatFinder = () => {
+        Object.keys(state.usedChats).forEach( key => {
+            if(envelope.to == key) {
+                state.usedChats[key].push(envelope.msg);
+                if (state.activeChat != envelope.to) {
+                    $(`#${envelope.from}`).classList.add('unread');
+                }
+                return 'found';
+            }
+        }
+    )}
+
+//* if it is not in the used chats put it
+    if (chatFinder() != 'found') {
+        state.usedChats[envelope.to] = [];
+        state.usedChats[envelope.to].push(envelope.msg);
+        if (state.activeChat != envelope.to) {
+            $(`#${envelope.from}`).classList.add('unread');
+        }
+    }
+    
+    if (state.activeChat == envelope.to) {
+        document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<li>${envelope.msg}</li>`);
+    } 
+});
+
+socket.on('users', users => {
+    state.users = users;
+    listManager(users, 'users-list');
+});
+
+socket.on('channels', channels => {
+    state.channels = channels;
+    listManager(channels, 'channels-list');
+});
 
 socket.emit('token', sessionStorage.getItem('token'));
 
 socket.on('ticket', ticket => {
-    document
-    .querySelector('#chat-box')
-    .insertAdjacentHTML("beforeend", `<p class="chat-welcome slide-top">Welcome on board ${ticket.nickName}</p>`);
+    $('#chat-box')
+    .insertAdjacentHTML("beforebegin", `<p class="chat-welcome slide-top">Today you are ${ticket.nickName}</p>`);
+    state.currentUser = ticket.nickName;
     sessionStorage.setItem('token', ticket.token);
 });
-
-socket.on('chat msg', msg => {
-    document.querySelector('#chat-box').insertAdjacentHTML("beforeend", `<p class="chat-msg"> ${msg}</p>`);
-});
-
